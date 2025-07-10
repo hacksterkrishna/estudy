e1789b63f9c54f56629f50c2663a7d67d4b4f92d
hacksterkrishna <krishnagek@gmail.com>
2025-07-11 01:06:14 +0545

    update

--- includes/connect.php
+++ includes/connect.php
@@ -1,8 +1,8 @@
 <?php
 // includes/connect.php
 $host = 'localhost';
-$user = 'root';       // change this if you have a different DB user
-$pass = '';           // change this if you have a DB password
+$user = 'hackster';       // change this if you have a different DB user
+$pass = 'hackster@#$123';           // change this if you have a DB password
 $dbname = 'estudy_skillzennepal';
 
 $conn = new mysqli($host, $user, $pass, $dbname);
